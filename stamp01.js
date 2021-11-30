8588da9be1c44d0769dc022403e735c9638c3049
jesterkaz <haru20011014@yahoo.co.jp>
2021-11-30 11:44:30 +0900

    スタンプ処理-変数書き込み

--- stamp01.js
+++ stamp01.js
@@ -2,3 +2,13 @@ setTimeout("link()", 10*1000);
 function link(){
 location.href='index.html';
 }
+
+$(function(){	
+	//Cookieの読み込み
+	var stamp01 = $.cookie('stamp01');
+    //訪問数のカウント
+    if(stamp01 == null){ //最初の訪問
+        stamp01 = 1;
+    }
+    $.cookie('stamp01', stamp01, {expires: 7});
+});
